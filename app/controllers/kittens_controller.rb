61154a9cab6958b213d10c062700efdaf407a2bb
Aripov-Sirojiddin <aripov.2@osu.edu>
2024-12-20 15:20:46 -0500

    Can now respond to JSON requests

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,10 +1,20 @@
 class KittensController < ApplicationController
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kittens }
+    end
   end
 
   def show
     @kitten = Kitten.find_by(id: params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @kitten }
+    end
   end
 
   def new
--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,5 @@
 class Kitten < ApplicationRecord
+  def as_json(options = {})
+    super({ only: [ :name, :cuteness, :softness ] }.merge(options))
+  end
 end
